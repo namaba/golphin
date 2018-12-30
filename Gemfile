2c432d3b8b595cadde042befe4e512489844e190
namaba <one.2426@gmail.com>
2018-12-30 13:23:50 +0900

    feat:pry_byebug追加

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'pry-byebug'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,12 @@ module Golphin
     config.load_defaults 5.2
     config.generators.template_engine = :slim
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_tiemzone = :local
+
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
