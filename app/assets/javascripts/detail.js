f73caaf5581a8912fb646d5fbe05c103c78d3b2a
Shota <t.shota23@gmail.com>
2019-05-09 20:08:49 +0900

    詳細ページテストアップ

--- app/assets/javascripts/detail.js
+++ app/assets/javascripts/detail.js
@@ -4,13 +4,40 @@ $(function() {
 
   tab.on('click',function(){
     var id = $(this).attr('id');
-    console.log(id);
     tab.removeClass('active');
     $(this).addClass('active');
   });
 
-$(function(){
-  $('#dateList').load('reviews.html.slim');
+// function showUp(tab_name,content){
+//   if($('#review_tab').hasClass('active')){
+//     $('#comment').fadeIn();
+//   } else {
+//     $('#comment').fadeOut();
+//   }
+// }
+function tab_switch() {
+  $('.tab').find('li').on('click',function(){
+    var className = $(this).attr("id");
+    if (className=="plan_tab"){
+      $('#contentsBottom').fadeIn().addClass('show');
+      $('.panel').not('#contentsBottom').fadeOut();
+      console.log('click');
+    } else if (className=="clubInfo_tab") {
+      $('#courseInfo').fadeIn().addClass('show');
+      $('.panel').not('#courseInfo').fadeOut();
+    } else if (className=="access_tab") {
+      $('#access').fadeIn().addClass('show');
+      $('.panel').not('#access').fadeOut();
+    } else if (className=="review_tab") {
+      $('#comment').fadeIn().addClass('show');
+      $('.panel').not('#comment').fadeOut();
+    }
+  });
+}
+tab_switch();
 });
-
-});
+// $('.area').hover(function(){
+// 		$(this).find('.is-show_item a').css('colr','red');
+// 		$(this).next('.area-list').slideDown();
+// 		$('.area').not(this).next('.area-list').slideUp();
+// 	});
